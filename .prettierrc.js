547fe626c6984f6da59ce5f38e8e71250dca7e49
Titus Kirch <titus.kirch@kirch.dev>
2024-03-31 19:52:53 +0200

    Implement lint and format

---
+++ .prettierrc.js
@@ -0,0 +1,15 @@
+/** @type {import("prettier").Options} */
+const config = {
+  // plugins: [require('prettier-plugin-tailwindcss')],
+  // tailwindConfig: './packages/ui/tailwind.config.ts',
+  endOfLine: "lf",
+  printWidth: 100,
+  semi: true,
+  singleQuote: true,
+  tabWidth: 2,
+  trailingComma: "es5",
+  useTabs: false,
+  vueIndentScriptAndStyle: true,
+};
+
+module.exports = config;
--- apps/web/pages/index.vue
+++ apps/web/pages/index.vue
@@ -1,3 +1,3 @@
 <template>
-    <div>Blog</div>
+    <div>Web</div>
 </template>
