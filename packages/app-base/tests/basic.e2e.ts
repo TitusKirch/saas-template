3e831b80c6629b01a8409ad7c85def320b20ac50
Titus Kirch <titus.kirch@kirch.dev>
2024-04-27 14:45:02 +0200

    Fix e2e test

--- packages/app-base/tests/basic.e2e.ts
+++ packages/app-base/tests/basic.e2e.ts
@@ -2,5 +2,5 @@ import { expect, test } from '@nuxt/test-utils/playwright'
 
 test('Renders Welcome to Nuxt!', async ({ page, goto }) => {
   await goto('/', { waitUntil: 'hydration' })
-  await expect(page.getByRole('heading', { name: 'Welcome t2o Nuxt!' })).toBeVisible()
+  await expect(page.getByRole('heading', { name: 'Welcome to Nuxt!' })).toBeVisible()
 })
