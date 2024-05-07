8ceaa3d43185884faf95bf6f820f2059fc1ed5ba
Titus Kirch <titus.kirch@kirch.dev>
2024-05-07 20:22:22 +0200

    Disabled all

--- apps/blog/nuxt.config.ts
+++ apps/blog/nuxt.config.ts
@@ -1,14 +1,14 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  extends: ['../../packages/app-base'],
-  i18n: {
-    langDir: './locales',
-    lazy: true,
-    strategy: 'prefix_and_default',
-    defaultLocale: 'de-DE',
-    locales: [
-      { code: 'de-DE', iso: 'de-DE', files: ['de.json'] },
-      { code: 'en-GB', iso: 'en-GB', files: ['en.json'] },
-    ],
-  },
+  // extends: ['../../packages/app-base'],
+  // i18n: {
+  //   langDir: './locales',
+  //   lazy: true,
+  //   strategy: 'prefix_and_default',
+  //   defaultLocale: 'de-DE',
+  //   locales: [
+  //     { code: 'de-DE', iso: 'de-DE', files: ['de.json'] },
+  //     { code: 'en-GB', iso: 'en-GB', files: ['en.json'] },
+  //   ],
+  // },
 })
