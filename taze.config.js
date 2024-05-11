35ab55cc6366d31a60fb2e3bd200af7f5936e9f0
Titus Kirch <titus.kirch@kirch.dev>
2024-05-11 14:02:39 +0200

    Fix taze

--- taze.config.js
+++ taze.config.js
@@ -3,10 +3,6 @@ import { defineConfig } from 'taze'
 export default defineConfig({
   force: true,
   recursive: true,
-  exclude: [
-    // exclude so we stick on version 4.21.1 until following issues are resolved
-    // https://github.com/sidebase/nuxt-auth/issues/514
-    // https://github.com/sidebase/nuxt-auth/issues/518
-    'next-auth',
-  ],
+  exclude: [],
+  ignorePaths: ['services/**/vendor'],
 })
