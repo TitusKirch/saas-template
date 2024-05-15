255ce980f08bc82375a411be3ef85c1e57e9af6a
Titus Kirch <titus.kirch@tkirch.dev>
2024-05-15 23:38:29 +0200

    feat(api): disable fortify views

--- services/api/app/Models/User.php
+++ services/api/app/Models/User.php
@@ -7,11 +7,12 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Kra8\Snowflake\HasSnowflakePrimary;
+use Laravel\Sanctum\HasApiTokens;
 use Spatie\Permission\Traits\HasRoles;
 
 class User extends Authenticatable
 {
-    use HasFactory, HasRoles, HasSnowflakePrimary, Notifiable;
+    use HasApiTokens, HasFactory, HasRoles, HasSnowflakePrimary, Notifiable;
 
     /**
      * The attributes that are mass assignable.
--- services/api/config/fortify.php
+++ services/api/config/fortify.php
@@ -130,7 +130,7 @@ return [
     |
     */
 
-    'views' => true,
+    'views' => false,
 
     /*
     |--------------------------------------------------------------------------
@@ -146,7 +146,7 @@ return [
     'features' => [
         Features::registration(),
         Features::resetPasswords(),
-        // Features::emailVerification(),
+        Features::emailVerification(),
         Features::updateProfileInformation(),
         Features::updatePasswords(),
         Features::twoFactorAuthentication([
