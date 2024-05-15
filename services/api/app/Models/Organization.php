9dfbff250be3325446ab8a6969f4fe21e34ee8a5
Titus Kirch <titus.kirch@tkirch.dev>
2024-05-15 20:28:09 +0200

    feat(api): implement roles for organizations

--- services/api/app/Models/Organization.php
+++ services/api/app/Models/Organization.php
@@ -9,4 +9,75 @@ use Kra8\Snowflake\HasSnowflakePrimary;
 class Organization extends Model
 {
     use HasFactory, HasSnowflakePrimary;
+
+    /**
+     * @inheritDoc
+     */
+    protected $fillable = [
+        'name',
+    ];
+
+    /** 
+     * @inheritdoc
+     */
+    protected static function boot()
+    {
+        parent::boot();
+
+        self::created(function ($organization) {
+            Role::create([
+                'organization_id' => $organization->id,
+                'name' => 'Owner',
+                'is_owner' => true,
+            ]);
+
+            Role::create([
+                'organization_id' => $organization->id,
+                'name' => 'Member',
+                'is_default' => true,
+            ]);
+        });
+    }
+
+    /**
+     * Get the roles for the organization.
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function roles()
+    {
+        return $this->hasMany(Role::class);
+    }
+
+    /**
+     * Get the owner role for the organization.
+     *
+     * @return \App\Models\Role
+     */
+    public function ownerRole()
+    {
+        return $this->roles()->where('is_owner', true)->first();
+    }
+
+    /**
+     * Get the default role for the organization.
+     * 
+     * @return \App\Models\Role
+     */
+    public function defaultRole()
+    {
+        return $this->roles()->where('is_default', true)->first();
+    }
+
+    /**
+     * Get the users for the organization.
+     *
+     * @return \Illuminate\Database\Eloquent\Collection
+     */
+    public function users()
+    {
+        return $this->roles()->with('users')->get()->flatMap(function ($role) {
+            return $role->users;
+        })->unique('id');
+    }
 }
