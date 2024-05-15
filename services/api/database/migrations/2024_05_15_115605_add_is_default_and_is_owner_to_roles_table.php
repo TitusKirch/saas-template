c61b548e83e0f91626dc1bcdb3568075e62ef639
Titus Kirch <titus.kirch@tkirch.dev>
2024-05-15 12:04:28 +0200

    feat(api): add is default and is owner to roles table

---
+++ services/api/database/migrations/2024_05_15_115605_add_is_default_and_is_owner_to_roles_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        $tableNames = config('permission.table_names');
+
+        if (empty($tableNames)) {
+            throw new \Exception('Error: config/permission.php not loaded. Run [php artisan config:clear] and try again.');
+        }
+
+        Schema::table($tableNames['roles'], function (Blueprint $table) {
+            $table->boolean('is_default')->default(false);
+            $table->boolean('is_owner')->default(false);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        $tableNames = config('permission.table_names');
+
+        if (empty($tableNames)) {
+            throw new \Exception('Error: config/permission.php not found and defaults could not be merged. Please publish the package configuration before proceeding, or drop the tables manually.');
+        }
+
+        Schema::table($tableNames['roles'], function (Blueprint $table) {
+            $table->dropColumn('is_default');
+            $table->dropColumn('is_owner');
+        });
+    }
+};
