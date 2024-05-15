e6a38cfa1de262a51d822e97255fa77337d30561
Titus Kirch <titus.kirch@tkirch.dev>
2024-05-15 20:28:37 +0200

    feat(api): add a first basic debug route for testing

--- services/api/routes/web.php
+++ services/api/routes/web.php
@@ -4,3 +4,4 @@ use App\Http\Controllers\AppController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [AppController::class, 'index']);
+Route::get('/debug', [AppController::class, 'debug']);
