88999eadf794f005bfaaecefd1985402671a78f6
Titus Kirch <titus.kirch@kirch.dev>
2024-05-30 00:48:47 +0200

    fix(api): remove unneeded provider registration

--- services/api/bootstrap/app.php
+++ services/api/bootstrap/app.php
@@ -2,15 +2,11 @@
 
 use App\Http\Middleware\AcceptLanguage;
 use App\Http\Middleware\OrganizationsPermission;
-use App\Providers\HealthServiceProvider;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 
 return Application::configure(basePath: dirname(__DIR__))
-    ->withProviders([
-        HealthServiceProvider::class,
-    ])
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
         commands: __DIR__.'/../routes/console.php',
