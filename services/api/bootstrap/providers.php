9f64eb22b45c6252e3913b58b66ec54ebaf220da
Titus Kirch <titus.kirch@kirch.dev>
2024-05-30 01:50:24 +0200

    featt(api): basic overwrite of emails for fortify

--- services/api/bootstrap/providers.php
+++ services/api/bootstrap/providers.php
@@ -2,6 +2,7 @@
 
 return [
     App\Providers\AppServiceProvider::class,
+    App\Providers\AuthServiceProvider::class,
     App\Providers\FortifyServiceProvider::class,
     App\Providers\HealthServiceProvider::class,
     App\Providers\TelescopeServiceProvider::class,
