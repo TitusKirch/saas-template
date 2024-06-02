af821981ebcb64f08caefd9a2cf0dfb079ddc4f8
Titus Kirch <titus.kirch@kirch.dev>
2024-06-02 15:13:09 +0200

    fix(apps): checkbox and toggle style

--- packages/app-base/formkitTheme.ts
+++ packages/app-base/formkitTheme.ts
@@ -3,4 +3,10 @@ export const classes = {
     suffixIcon:
       'hover:text-primary-600 dark:hover:text-primary-500 group-data-[disabled]:pointer-events-none',
   },
+  toggle: {
+    track: 'peer-checked:[&>div:last-child]:left-[calc(100%-1.25rem)]',
+  },
+  checkbox: {
+    decoratorIcon: '!top-0 !left-[calc(66%/(2*2))]',
+  },
 };
