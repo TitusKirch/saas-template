d87667dad5ddbb655b1c79006a0e4695c9713921
Titus Kirch <titus.kirch@kirch.dev>
2024-06-11 21:29:38 +0200

    feat(apps): add eslint-plugin-tailwindcss

--- apps/blog/eslint.config.mjs
+++ apps/blog/eslint.config.mjs
@@ -1,3 +1,4 @@
 import withNuxt from './.nuxt/eslint.config.mjs';
+import tailwind from 'eslint-plugin-tailwindcss';
 
-export default withNuxt();
+export default withNuxt(tailwind.configs['flat/recommended']);
