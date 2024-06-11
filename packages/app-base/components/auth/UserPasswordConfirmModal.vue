83084ca53f3f06563e0e48515e40a68b247d4fe6
Titus Kirch <titus.kirch@kirch.dev>
2024-06-11 23:46:18 +0200

    fix(apps): error messages are shown even if no error is there

--- packages/app-base/components/auth/UserPasswordConfirmModal.vue
+++ packages/app-base/components/auth/UserPasswordConfirmModal.vue
@@ -44,7 +44,7 @@
   watch(form, (newValue: Form, oldValue: Form) => {
     const updatedErrorMessages: typeof errorMessages.value = {};
     for (const key of Object.keys(newValue) as Array<keyof Form>) {
-      if (newValue[key] !== oldValue[key]) {
+      if (newValue[key] !== oldValue[key] && errorMessages.value[key]) {
         updatedErrorMessages[key] = errorMessages.value[key];
       }
     }
