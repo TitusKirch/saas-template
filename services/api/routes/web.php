e77671b0019e489b71d15830ffd86ab191a7a717
Titus Kirch <titus.kirch@kirch.dev>
2024-06-23 23:26:55 +0200

    feat(api): remove dev routes

--- services/api/routes/web.php
+++ services/api/routes/web.php
@@ -1,13 +1,10 @@
 <?php
 
-use App\Http\Controllers\AppController;
 use App\Http\Controllers\AuthProviderController;
 use App\Http\Controllers\FeatureController;
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', [AppController::class, 'index'])->name('app.index');
-Route::get('/debug', [AppController::class, 'debug'])->name('app.debug');
 Route::get('/health', \Spatie\Health\Http\Controllers\HealthCheckResultsController::class)->name('health');
 Route::get('/health/json', \Spatie\Health\Http\Controllers\HealthCheckJsonResultsController::class)->name('health.json');
 
