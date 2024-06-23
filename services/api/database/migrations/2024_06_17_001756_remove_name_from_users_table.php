3b5c38bebb8f93152fcc05e733981e8e6597af02
Titus Kirch <titus.kirch@kirch.dev>
2024-06-23 21:36:08 +0200

    feat(api): improve migrations

--- services/api/database/migrations/2024_06_17_001756_remove_name_from_users_table.php
+++ services/api/database/migrations/2024_06_17_001756_remove_name_from_users_table.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
@@ -22,7 +23,18 @@ return new class extends Migration
     public function down(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->string('name');
+            $table->string('name')->nullable()->after('id');
+        });
+
+        // loop through all users and set the name by first name and last name
+        $users = DB::table('users')->get();
+        foreach ($users as $user) {
+            $name = $user->first_name.' '.$user->last_name;
+            DB::table('users')->where('id', $user->id)->update(['name' => $name]);
+        }
+
+        Schema::table('users', function (Blueprint $table) {
+            $table->string('name')->nullable(false)->change();
         });
     }
 };
