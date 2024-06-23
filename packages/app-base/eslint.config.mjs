263cc10de155da0436825f4830f68e789c635b93
Titus Kirch <titus.kirch@kirch.dev>
2024-06-24 01:39:09 +0200

    feat(apps): disable vue/html-self-closing

--- packages/app-base/eslint.config.mjs
+++ packages/app-base/eslint.config.mjs
@@ -1,16 +1,23 @@
 import withNuxt from './.nuxt/eslint.config.mjs';
 import tailwind from 'eslint-plugin-tailwindcss';
 
-export default withNuxt(tailwind.configs['flat/recommended']).override('tailwindcss:rules', {
-  rules: {
-    'tailwindcss/no-custom-classname': [
-      'warn',
-      {
-        whitelist: [
-          '^!?(bg|border|ring|text)-primary-(50|100|200|300|400|500|600|700|800|900|950)$',
-          'formkit-inner',
-        ],
-      },
-    ],
-  },
-});
+export default withNuxt(tailwind.configs['flat/recommended'])
+  .override('tailwindcss:rules', {
+    rules: {
+      'tailwindcss/no-custom-classname': [
+        'warn',
+        {
+          whitelist: [
+            '^!?(bg|border|ring|text)-primary-(50|100|200|300|400|500|600|700|800|900|950)$',
+            'formkit-inner',
+          ],
+        },
+      ],
+    },
+  })
+  .override('nuxt:vue', {
+    rules: {
+      // this rule is in conflict with prettier
+      'vue/html-self-closing': 0,
+    },
+  });
