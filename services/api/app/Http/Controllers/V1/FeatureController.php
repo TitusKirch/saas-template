3a787326e9a4f30cb6a59c4435453cf63b5c088e
Titus Kirch <titus.kirch@kirch.dev>
2024-07-14 16:14:42 +0200

    refactor(api): adjust resources namespace

--- services/api/app/Http/Controllers/V1/FeatureController.php
+++ services/api/app/Http/Controllers/V1/FeatureController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers\V1;
 
 use App\Http\Controllers\Controller;
-use App\Http\Resources\FeatureResource;
+use App\Http\Resources\V1\FeatureResource;
 use Laravel\Pennant\Feature;
 
 class FeatureController extends Controller
--- services/api/app/Http/Controllers/V1/UserController.php
+++ services/api/app/Http/Controllers/V1/UserController.php
@@ -3,8 +3,8 @@
 namespace App\Http\Controllers\V1;
 
 use App\Http\Controllers\Controller;
-use App\Http\Resources\UserMeResource;
-use App\Http\Resources\UserResource;
+use App\Http\Resources\V1\UserMeResource;
+use App\Http\Resources\V1\UserResource;
 use App\Models\User;
 use Illuminate\Http\Request;
 
