09f22cd35082a9da52f96f9eb6cae7e7e5dc42fd
Titus Kirch <titus.kirch@kirch.dev>
2024-07-24 20:47:55 +0200

    feat(api): implement ConfirmablePresignedUrlResource

--- services/api/app/Http/Resources/V1/ConfirmablePresignedUrlResource.php
+++ services/api/app/Http/Resources/V1/ConfirmablePresignedUrlResource.php
@@ -5,7 +5,7 @@ namespace App\Http\Resources\V1;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 
-class PresignedUrlResource extends JsonResource
+class ConfirmablePresignedUrlResource extends JsonResource
 {
     /**
      * {@inheritdoc}
@@ -13,6 +13,7 @@ class PresignedUrlResource extends JsonResource
     public function toArray(Request $request): array
     {
         return [
+            'confirmationUrl' => $this->confirmationUrl,
             'presignedUrl' => $this->presignedUrl,
         ];
     }
